eefa32a957ee1477d4350cd34bcbbde6f081c2dd
Danilo Tuler <danilo@cartesi.io>
2021-05-11 23:17:25 -0400

    Template project for onchain code using hardhat

---
+++ README.md
@@ -0,0 +1,52 @@
+> :warning: The Cartesi team keeps working internally on the next version of this repository, following its regular development roadmap. Whenever there's a new version ready or important fix, these are published to the public source tree as new releases.
+
+# Template Project
+
+THIS IS A TEMPLATE PROJECT.
+
+It includes the following feature out of the box:
+
+- Deployment with hardhat-deploy
+- TypeChain, including npm packaging
+- Code formatting using prettier, and CI enforcement
+- ethereum-waffle for testing
+
+Packaged as npm and published at https://www.npmjs.com/package/@cartesi/<module>
+
+The npm package contains `hardhat` deployment artifacts with information of deployed contracts on the following testnets:
+- Rinkeby
+- Kovan
+- Goerli
+- Matic Testnet
+- Binance Smart Chain Testnet
+- Avalanche
+
+## Getting Started
+
+To use this package in another project you must include as a dependency in `package.json`.
+
+    "dependencies": {
+        "@cartesi/<module>": "<version>"
+    },
+
+And in your solidity contract import it as:
+
+    import "@cartesi/<module>/contracts/<contract>.sol";
+
+## Build
+
+To get a list of all available `yarn` targets run:
+
+```shell
+% yarn run info
+```
+
+## Contributing
+
+Thank you for your interest in Cartesi! Head over to our [Contributing Guidelines](CONTRIBUTING.md) for instructions on how to sign our Contributors Agreement and get started with Cartesi!
+
+Please note we have a [Code of Conduct](CODE_OF_CONDUCT.md), please follow it in all your interactions with the project.
+
+## License
+The solidity-util repository and all contributions are licensed under
+[APACHE 2.0](https://www.apache.org/licenses/LICENSE-2.0). Please review our [LICENSE](LICENSE) file.
---
+++ hardhat.config.ts
@@ -0,0 +1,92 @@
+// Copyright 2021 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the license at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import { HardhatUserConfig } from "hardhat/config";
+import { HttpNetworkUserConfig } from "hardhat/types";
+
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+
+// read MNEMONIC from file or from env variable
+let mnemonic = process.env.MNEMONIC;
+
+const infuraNetwork = (
+    network: string,
+    chainId?: number,
+    gas?: number
+): HttpNetworkUserConfig => {
+    return {
+        url: `https://${network}.infura.io/v3/${process.env.PROJECT_ID}`,
+        chainId,
+        gas,
+        accounts: mnemonic ? { mnemonic } : undefined,
+    };
+};
+
+const config: HardhatUserConfig = {
+    networks: {
+        hardhat: mnemonic ? { accounts: { mnemonic } } : {},
+        localhost: {
+            url: "http://localhost:8545",
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
+        rinkeby: infuraNetwork("rinkeby", 4, 6283185),
+        kovan: infuraNetwork("kovan", 42, 6283185),
+        goerli: infuraNetwork("goerli", 5, 6283185),
+        mainnet: infuraNetwork("mainnet", 1, 6283185),
+        matic_testnet: {
+            url: "https://rpc-mumbai.matic.today",
+            chainId: 80001,
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
+        bsc_testnet: {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+            chainId: 97,
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
+        avax_testnet: {
+            url: "https://api.avax-test.network/ext/bc/C/rpc",
+            chainId: 0xa869,
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.4",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                    },
+                },
+            },
+        ],
+    },
+    paths: {
+        artifacts: "artifacts",
+        deploy: "deploy",
+        deployments: "deployments",
+    },
+    typechain: {
+        outDir: "src/types",
+        target: "ethers-v5",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+};
+
+export default config;
