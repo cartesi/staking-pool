33cb0fd482c6e1464215d1a8028fab2203bb8392
Danilo Tuler <danilo@cartesi.io>
2021-07-05 14:32:16 -0400

    Decoupling reference pool deployment and factory deployment

--- deploy/02_factory.ts
+++ deploy/02_factory.ts
@@ -12,10 +12,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
-const MINUTE = 60; // seconds in a minute
-const HOUR = 60 * MINUTE; // seconds in an hour
-const DAY = 24 * HOUR; // seconds in a day
-
 /**
  * Deploy a mock contract for a chainlink aggregator, where anyone can set the value.
  * @param hre hardhat environment
@@ -34,39 +30,51 @@ const deployChainlink = async (
     return deployment.address;
 };
 
-const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+/**
+ * Deploy a mock contract for uniswap pair, where anyone can set price.
+ * @param hre hardhat environment
+ * @returns contract address
+ */
+const deployUniswap = async (
+    hre: HardhatRuntimeEnvironment
+): Promise<string> => {
     const { deployments, getNamedAccounts, ethers, network } = hre;
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
-    const { CartesiToken, PoS, StakingImpl, WorkerManagerAuthManagerImpl } =
-        await deployments.all();
+    const deployment = await deploy("MockUniswapV2Pair", {
+        from: deployer,
+        log: true,
+    });
+    return deployment.address;
+};
 
-    const ethNetwork = await ethers.getDefaultProvider().getNetwork();
-    const ensAddress = ethNetwork.ensAddress || ethers.constants.AddressZero;
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts, network } = hre;
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const { StakingPoolImpl } = await deployments.all();
 
-    // deploy reference pool
-    const timeToStake =
-        network.name == "mainnet" || network.name == "ropsten"
-            ? 6 * HOUR
-            : 2 * MINUTE;
-    const timeToRelease =
-        network.name == "mainnet" || network.name == "ropsten"
-            ? 2 * DAY
-            : 2 * MINUTE;
-    await deploy("StakingPoolImpl", {
-        args: [
-            CartesiToken.address,
-            StakingImpl.address,
-            PoS.address,
-            timeToStake,
-            timeToRelease,
-            ensAddress,
-            WorkerManagerAuthManagerImpl.address,
-        ],
+    network.config.chainId === 1;
+
+    const chainlinkOracle =
+        network.config.chainId === 1
+            ? "0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C" // https://data.chain.link/fast-gas-gwei
+            : await deployChainlink(hre);
+
+    const uniswapOracle =
+        network.config.chainId === 1
+            ? "0x58EEB5D44Dc41965AB0a9E563536175C8dc5C3B3" // https://v2.info.uniswap.org/pair/0x58eeb5d44dc41965ab0a9e563536175c8dc5c3b3
+            : await deployUniswap(hre);
+    // XXX: Uniswap V3 is at https://info.uniswap.org/#/pools/0x01949723055a451229c7ba3a817937c966748f76
+
+    // deploy pool factory
+    await deploy("StakingPoolFactoryImpl", {
+        args: [StakingPoolImpl.address, chainlinkOracle, uniswapOracle],
         from: deployer,
         log: true,
     });
 };
 
 export default func;
-export const tags = ["Pool"];
+export const tags = ["PoolFactory"];
+export const dependencies = ["Pool"];
