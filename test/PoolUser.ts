eb90411ffd4d1a4f6ed10cd3df5850e2e520cb7d
Gabriel Barros <gabriel.barros@cartesi.io>
2021-07-16 14:45:35 +0000

    Move setupPool to utils

--- test/PoolUser.ts
+++ test/PoolUser.ts
@@ -10,157 +10,63 @@
 // specific language governing permissions and limitations under the License.
 
 import { expect, use } from "chai";
-import { deployments, ethers, waffle } from "hardhat";
-import { JsonRpcProvider } from "@ethersproject/providers";
+import { ethers, waffle } from "hardhat";
 
-import { CartesiToken__factory } from "@cartesi/token";
-import { StakingPoolImpl__factory } from "../src/types/factories/StakingPoolImpl__factory";
-
-import { advanceTime, setNextBlockTimestamp } from "./utils";
-import { PoS__factory } from "@cartesi/pos";
-const { solidity, deployMockContract } = waffle;
+import {
+    advanceTime,
+    setNextBlockTimestamp,
+    setupPool,
+    parseCTSI,
+} from "./utils";
+const { solidity } = waffle;
 
 use(solidity);
 const MINUTE = 60; // seconds in a minute
 const HOUR = 60 * MINUTE; // seconds in an hour
 const STAKE_LOCK = 60; // seconds
 
-const parseCTSI = (value: string) => ethers.utils.parseUnits(value, 18);
+// const  = (value: string) => ethers.utils.parseUnits(value, 18);
 
 describe("StakingPoolUser", async () => {
     beforeEach(async () => {});
 
-    const setupPool = deployments.createFixture(
-        async ({ deployments, ethers }, options) => {
-            // start with a fresh deployment
-            await deployments.fixture();
-
-            const [deployer, alice, bob] = await ethers.getSigners();
-            const {
-                BlockSelector,
-                CartesiToken,
-                StakingImpl,
-                StakingPoolImpl,
-                WorkerManagerAuthManagerImpl,
-            } = await deployments.all();
-
-            // setup fee mock contract
-            const reward = parseCTSI("2900");
-            const commission = reward.div(10); // 10%
-            const commissionArtifact = await deployments.getArtifact(
-                "FlatRateCommission"
-            );
-            const fee = await deployMockContract(
-                deployer,
-                commissionArtifact.abi
-            );
-            await fee.mock.getCommission.returns(commission);
-
-            // setup token distribution
-            const token = CartesiToken__factory.connect(
-                CartesiToken.address,
-                deployer
-            );
-            // send 10k tokens to alice and bob
-            await token.transfer(alice.address, parseCTSI("10000"));
-            await token.transfer(bob.address, parseCTSI("10000"));
-
-            const pool = StakingPoolImpl__factory.connect(
-                StakingPoolImpl.address,
-                deployer
-            );
-            await pool.initialize(fee.address, STAKE_LOCK);
-
-            // deploy a mock BlockSelector that always returns true to produceBlock
-            const blockSelector = await deployMockContract(
-                deployer,
-                BlockSelector.abi
-            );
-            blockSelector.mock.instantiate.returns(0);
-            blockSelector.mock.produceBlock.returns(true);
-            blockSelector.mock.canProduceBlock.returns(true);
-            await pool.selfhire({ value: ethers.utils.parseEther("0.001") });
-
-            // instantiate a chain
-            const pos = PoS__factory.connect(await pool.pos(), deployer);
-            await pos.instantiate(
-                StakingImpl.address,
-                blockSelector.address,
-                WorkerManagerAuthManagerImpl.address,
-                "1000000000",
-                "100000000000000000000000000",
-                50000,
-                56,
-                CartesiToken.address,
-                reward,
-                reward,
-                10000,
-                10000
-            );
-
-            // feed the RewardManager with 10 block worth of tokens
-            await token.transfer(
-                await pos.getRewardManagerAddress(0),
-                reward.mul(10)
-            );
-
-            return {
-                owner: {
-                    address: deployer.address,
-                    pool: pool,
-                    token: token,
-                },
-                alice: {
-                    address: alice.address,
-                    pool: pool.connect(alice),
-                    token: token.connect(alice),
-                },
-                bob: {
-                    address: bob.address,
-                    pool: pool.connect(bob),
-                    token: token.connect(bob),
-                },
-            };
-        }
-    );
-
     it("should not allow unstake of zero shares", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         await expect(alice.pool.unstake(0)).to.revertedWith(
             "StakingPoolUserImpl: invalid amount of shares"
         );
     });
 
     it("should not allow unstake with no shares", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         await expect(alice.pool.unstake(1)).to.revertedWith(
             "StakingPoolUserImpl: insufficient shares"
         );
     });
 
     it("should not allow stake zero tokens", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         await expect(alice.pool.stake(0)).to.revertedWith(
             "StakingPoolUserImpl: amount must be greater than 0"
         );
     });
 
     it("should not stake what user don't have", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         await expect(alice.pool.stake(parseCTSI("100000"))).to.be.revertedWith(
             "ERC20: transfer amount exceeds balance"
         );
     });
 
     it("should not stake what user didn't allow", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         await expect(alice.pool.stake(parseCTSI("1000"))).to.be.revertedWith(
             "ERC20: transfer amount exceeds allowance"
         );
     });
 
     it("should successfully stake", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         const stake = parseCTSI("1000");
         await alice.token.approve(alice.pool.address, stake);
 
@@ -182,7 +88,7 @@ describe("StakingPoolUser", async () => {
     });
 
     it("should not allow to receive zero shares", async () => {
-        const { alice, bob, owner } = await setupPool();
+        const { alice, bob, owner } = await setupPool({ STAKE_LOCK });
 
         // stake 1e-18 tokens, earn 1e-18 shares
         await alice.token.approve(alice.pool.address, 1);
@@ -208,7 +114,7 @@ describe("StakingPoolUser", async () => {
     });
 
     it("should lock stake", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         const stake = parseCTSI("1000");
         await alice.token.approve(alice.pool.address, stake);
 
@@ -225,7 +131,7 @@ describe("StakingPoolUser", async () => {
     });
 
     it("should successfully unstake", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         const stake = parseCTSI("1000");
         const unstake = stake.div(4);
         await alice.token.approve(alice.pool.address, stake);
@@ -255,14 +161,14 @@ describe("StakingPoolUser", async () => {
     });
 
     it("should not withdraw with no user balance", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         await expect(alice.pool.withdraw()).to.be.revertedWith(
             "StakingPoolUserImpl: no balance to withdraw"
         );
     });
 
     it("should withdraw right after unstake", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         const stake = parseCTSI("1000");
         const unstake = stake.div(4);
         await alice.token.approve(alice.pool.address, stake);
@@ -291,7 +197,7 @@ describe("StakingPoolUser", async () => {
     });
 
     it("should not withdraw with no pool balance", async () => {
-        const { alice } = await setupPool();
+        const { alice } = await setupPool({ STAKE_LOCK });
         const stake = parseCTSI("1000");
         const unstake = stake.div(4);
         await alice.token.approve(alice.pool.address, stake);
--- test/utils.ts
+++ test/utils.ts
@@ -10,9 +10,17 @@
 // specific language governing permissions and limitations under the License.
 
 import { use } from "chai";
-import { solidity } from "ethereum-waffle";
+import { deployments, ethers, waffle } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { JsonRpcProvider } from "@ethersproject/providers";
 import { Provider } from "@ethersproject/abstract-provider";
+
+import { PoS__factory } from "@cartesi/pos";
+import { StakingPoolImpl__factory } from "../src/types/factories/StakingPoolImpl__factory";
+import { CartesiToken__factory } from "@cartesi/token";
+
+const { solidity, deployMockContract } = waffle;
+export const { parseEther: parseCTSI } = ethers.utils;
 
 use(solidity);
 
@@ -36,3 +44,99 @@ export const advanceMultipleBlocks = async (
         await advanceBlock(provider);
     }
 };
+
+export const setupPool = deployments.createFixture(
+    async (
+        { deployments, ethers }: HardhatRuntimeEnvironment,
+        { STAKE_LOCK }: { STAKE_LOCK: number } = { STAKE_LOCK: 60 }
+    ) => {
+        // const op = Object.assign({}, options);
+        // const STAKE_LOCK = op.STAKE_LOCK ? op.STAKE_LOCK : 60;
+        // start with a fresh deployment
+        await deployments.fixture();
+
+        const [deployer, alice, bob] = await ethers.getSigners();
+        const {
+            BlockSelector,
+            CartesiToken,
+            StakingImpl,
+            StakingPoolImpl,
+            WorkerManagerAuthManagerImpl,
+        } = await deployments.all();
+
+        // setup fee mock contract
+        const reward = parseCTSI("2900");
+        const commission = reward.div(10); // 10%
+        const commissionArtifact = await deployments.getArtifact(
+            "FlatRateCommission"
+        );
+        const fee = await deployMockContract(deployer, commissionArtifact.abi);
+        await fee.mock.getCommission.returns(commission);
+
+        // setup token distribution
+        const token = CartesiToken__factory.connect(
+            CartesiToken.address,
+            deployer
+        );
+        // send 10k tokens to alice and bob
+        await token.transfer(alice.address, parseCTSI("10000"));
+        await token.transfer(bob.address, parseCTSI("10000"));
+
+        const pool = StakingPoolImpl__factory.connect(
+            StakingPoolImpl.address,
+            deployer
+        );
+        await pool.initialize(fee.address, STAKE_LOCK);
+
+        // deploy a mock BlockSelector that always returns true to produceBlock
+        const blockSelector = await deployMockContract(
+            deployer,
+            BlockSelector.abi
+        );
+        blockSelector.mock.instantiate.returns(0);
+        blockSelector.mock.produceBlock.returns(true);
+        blockSelector.mock.canProduceBlock.returns(true);
+        await pool.selfhire({ value: ethers.utils.parseEther("0.001") });
+
+        // instantiate a chain
+        const pos = PoS__factory.connect(await pool.pos(), deployer);
+        await pos.instantiate(
+            StakingImpl.address,
+            blockSelector.address,
+            WorkerManagerAuthManagerImpl.address,
+            "1000000000",
+            "100000000000000000000000000",
+            50000,
+            56,
+            CartesiToken.address,
+            reward,
+            reward,
+            10000,
+            10000
+        );
+
+        // feed the RewardManager with 10 block worth of tokens
+        await token.transfer(
+            await pos.getRewardManagerAddress(0),
+            reward.mul(10)
+        );
+
+        return {
+            owner: {
+                address: deployer.address,
+                pool: pool,
+                token: token,
+            },
+            alice: {
+                address: alice.address,
+                pool: pool.connect(alice),
+                token: token.connect(alice),
+            },
+            bob: {
+                address: bob.address,
+                pool: pool.connect(bob),
+                token: token.connect(bob),
+            },
+        };
+    }
+);
